2ce2d77d200d265924c88fd3e83ce0ea0ccbbdb9
Abiat Nushafa <nushafa1@gmail.com>
2025-05-22 20:45:27 +0600

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,12 @@
+#Let's check the data types of different values
+a = 5
+print("Type of a: ", type(a))
+
+b = 2.5
+print("Type of b: ", type(b))
+
+c = "Coding"
+print("Type of c: ", type(c))
+
+d = True
+print("Type of d: ", type(d))
